7e0f06a124726b2b85b108c96ee06d73c31181ba
Rafael Beims <rafael@beims.me>
2019-07-31 20:32:31 +0200

    Add Xresources configuration (mainly for urxvt)

--- scripts/configure_home.sh
+++ scripts/configure_home.sh
@@ -5,4 +5,6 @@ echo -n "Configure vim..."
 cp -a $CDIR/vim/vimrc $HOME/.vimrc
 cp -a $CDIR/vim/dotvim $HOME/.vim
 echo "done"
-
+echo -n "Configure Xresources..."
+cp -a $CDIR/term/Xresources $HOME/.Xresources
+echo "done"
